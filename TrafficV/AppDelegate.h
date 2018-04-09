d340a069e6f529042bcdf2a5b430da3da28b7c6c
Stone <test@16youludemac-mini.local>
2018-04-09 09:05:04 +0800

    Initial Commit

---
+++ TrafficV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrafficV
+//
+//  Created by test on 2018/4/9.
+//  Copyright © 2018年 com.youlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrafficV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrafficV
+//
+//  Created by test on 2018/4/9.
+//  Copyright © 2018年 com.youlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
